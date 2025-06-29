16431ca051ad10b766ca044ba7faf37bdbeb293a
Abhimanyu Ahuja <abhimanyuahuja12@gmail.com>
2025-06-29 13:56:06 +0530

    setup AI background job for code generation

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,25 @@
 import { inngest } from "./client";
+import { gemini, createAgent } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
-  },
+
+  async ({ event }) => {
+    // Create a new agent with a system prompt using Gemini
+    const codeAgent = createAgent({
+      name: "codeAgent",
+      system: "You are an expert next.js developer . you write readable, maintanable code. You write simple Next.js and React snippets , you have write code",
+      model: gemini({
+        model: "gemini-1.5-flash", // or "gemini-1.5-flash" for faster responses
+      }),
+    });
+
+    const { output } = await codeAgent.run(
+      `Write the following snippet ${event.data.value}`
+    );
+    console.log(output);
+
+    return { output };
+  }
 );
